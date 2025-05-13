f5193a733e6f8f9d41eb52c8993ebe750358f713
pinkth3floyd <prakash@zeptosky.com>
2025-05-13 19:26:25 +0545

    index page fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Home from './TodoListClient';
 import { configureDependencies } from './core/config/di';
 import { GET_TODOS_USECASE } from './core/features/todo/domain/usecases/gettodo';
 import { Todo } from './core/features/todo/domain/entities/todo';
+import TodoList from './core/features/todo/presentation/components/TodoList';
 
 interface HomePageProps {
   initialTodos: Todo[];
@@ -14,5 +15,19 @@ export default async function HomePage() {
   const getTodosUseCase = container[GET_TODOS_USECASE];
   const initialTodos: Todo[] = await getTodosUseCase.execute();
 
-  return <Home initialTodos={initialTodos} />;
+  return (
+
+   <div className="min-h-screen p-6">
+      <header className="mb-8 text-center">
+        <h1 className="text-3xl font-bold mb-2">Clean Architecture Demo</h1>
+        <p className="text-muted-foreground">
+          With Dependency Injection in React
+        </p>
+      </header>
+      
+      <main className="max-w-md mx-auto">
+        <TodoList />
+      </main>
+    </div>
+  )
 }
